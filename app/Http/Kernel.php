86b13c69029c85b841da403ea71f0e5318f689b8
omarmakramelbaz-hub <omarmakramelbaz@gmail.com>
2026-09-20 06:19:15 +0300

    Ensure GO schema before GO API authentication

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
+            \App\Http\Middleware\EnsureGoSchema::class,
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
